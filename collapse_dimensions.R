85aa9500b042f44f9a90f5da72786d1a68fbbd83
iamihgam <maghimaimarcus@gmail.com>
2018-04-03 03:34:53 +0200

    spruned up

--- collapse_dimensions.R
+++ collapse_dimensions.R
@@ -12,17 +12,10 @@ collapse_dimensions <- function(X){
     #Z : Flatened 2d array
   
   #Required functions
-  ndim <- function(x){length(dim(x))} # number of dimensions
+  #ndim 
+  #arraysize 
   
-  arraysize <- function(x, margin){ # for dimensions  greater than 2 espfor 3d and 4d arrays
-    if (ndim(x) < 3) {warning ("Try nrows, ncols or length for matrices and vectors")}
-    else if(ndim(x) == 3 && margin == 1) {dimsize(x)/prod(nrow(x),ncol(x))}
-    else if(ndim(x) == 3 && margin == 2) {warning ("Array column dimensions unavailable")}
-    else if(ndim(x) == 4 && margin == 1) { dimsize(x)/prod(nrow(x),ncol(x),dim(x)[ndim(x)])}
-    else if(ndim(x) == 4 && margin == 2) { dimsize(x)/prod(nrow(x),ncol(x),dim(x)[ndim(x)-1])}
-    else {warning ("Array dimensions unavailable")}
-  }
-  
+  # For a 4d array convert to 3d array...
     if (ndim(X) == 4){
     org <-arraysize(X, 1)
     com <- arraysize(X, 2)
