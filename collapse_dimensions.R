555d91a9cc66aa495bb9458188d818a564f343dd
iamihgam <maghimaimarcus@gmail.com>
2018-04-03 04:02:34 +0200

    comments

--- collapse_dimensions.R
+++ collapse_dimensions.R
@@ -1,12 +1,10 @@
 collapse_dimensions <- function(X){
-  # collapse 3d or 4d array into matrices
-  # significant changes in the way 4d matrices are handled
+  # For a 4d array, array rows and array columns are combined to a 3d array
+  #Further, the 3d arrays , the rows and columns are further combined to a 3d array
+  # significant changes in the way 4d matrices are handled compared to python and matlab scripts
+  
   #Input
-  ----------
   #X : 3d or 4d array to be collapsed
-  #first2dimensions : Boolean : For 3d array, should the last two dimensions
-  #be flattened together (default) or should the first two be
-  #flattened together instead (=true)?
     
   #Output
     #Z : Flatened 2d array
@@ -20,13 +18,14 @@ collapse_dimensions <- function(X){
     org <-arraysize(X, 1)
     com <- arraysize(X, 2)
     ind <- nrow(X) 
-    X3d <- array(X, dim=c(com, ind, org*com))  #dropping the dimensions into 3 with nrow:com, ncol:ind
+    X3d <- array(X, dim=c(com, ind, org*com))  #converting 4d into  3d with nrow:com, ncol:ind, arraysize = org*com
     Y <- array(0, dim=c(ind, com,org * com))  #creating an empty array with nrow:ind, ncol:com, arraysize=org*com
     org_3d <- arraysize(X3d, 1)
     for (ORG  in seq_along(1:org_3d)){
-      Y[, ,ORG] = aperm(X3d[ , ,ORG])
-                        }
+      Y[, ,ORG] = aperm(X3d[ , ,ORG])                     }
     }
-    else:
+    else {Y = X}
+  # check the results for Z
+  
   
 }
